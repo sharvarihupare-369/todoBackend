98205b74bd662275a562ecbaa4d272ef0906da3c
sharvarihupare-369 <sharvarihupare15@gmail.com>
2023-08-06 20:59:53 +0530

    cors

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const jsonServer = require("json-server")
 const server = jsonServer.create()
 const router = jsonServer.router("db.json")
 const middleware = jsonServer.defaults()
+const cors = require("cors")
 require('dotenv').config();
 const port = process.env.PORT || 5000
 
+server.use(cors())
 server.use(middleware)
 server.use(router)
 
